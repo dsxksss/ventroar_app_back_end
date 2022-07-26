51dafa7dc45dc71972bc56caa021445e6feb348a
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-07-27 01:14:51 +0800

    add:GetUserInBox路由(用来获取该用户的收件箱内容) add:api说明文档

--- index.js
+++ index.js
@@ -77,12 +77,16 @@ const HASCONFIGF = () => {
 };
 
 if (HASENV()) {
-  console.log(`缺少所需的环境变量[${HASENV_ERROR_NAME}],请初始化环境变量再次启动服务...`);
+  console.log(
+    `缺少所需的环境变量[${HASENV_ERROR_NAME}],请初始化环境变量再次启动服务...`
+  );
   process.exit(1);
 }
 
 if (HASCONFIGF()) {
-  console.log(`配置文件default.json属性值[${HASCONFIGF_ERROR_NAME}],请检查配置文件再次启动服务...`);
+  console.log(
+    `配置文件default.json属性值[${HASCONFIGF_ERROR_NAME}],请检查配置文件再次启动服务...`
+  );
   process.exit(1);
 }
 
@@ -100,7 +104,7 @@ mongoose
   //如果没有的话就创建这个表
   .connect("mongodb://localhost/VentRoarAppApi")
   .then(() => console.log("Connect DataBase...... OK"))
-  .catch(err => {
+  .catch((err) => {
     console.log(`Could not connect to dataBase [ ${err} ] !!!`);
   });
 
@@ -118,7 +122,7 @@ if (config.get("runMode") === "production") {
   //读取安全keyfile
   const keyfile = {
     key: fs.readFileSync("ventroar.xyz.key"),
-    cert: fs.readFileSync("ventroar.xyz_bundle.crt")
+    cert: fs.readFileSync("ventroar.xyz_bundle.crt"),
   };
   //适合开发模式用的端口(default:2548)
   https
@@ -139,6 +143,7 @@ const uploadImg = require("./src/routers/uploadImg");
 const uploadAvatar = require("./src/routers/uploadAvatar");
 const reUserName = require("./src/routers/reUserName");
 const reEmail = require("./src/routers/reEmail");
+const getUserInBox = require("./src/routers/inBoxRouters/getUserInBox");
 
 //SM:中间件
 //数据转换成req.body的JSON
@@ -162,3 +167,4 @@ app.use("/uploadimg", uploadImg);
 app.use("/uploadavatar", uploadAvatar);
 app.use("/reusername", reUserName);
 app.use("/reemail", reEmail);
+app.use("/getuserinbox", getUserInBox);
---
+++ src/routers/inBoxRouters/getUserInBox.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const auth = require("../../middlewares/auth");
+
+const router = express.Router();
+const { UserDB } = require("../../databases/userDB");
+
+router.get("/", [auth], async (req, res) => {
+  let user = await UserDB.findById(req.userToken._id);
+  if (!user) {
+    res.status(404).send({ msg: "数据库不存在此用户" });
+  }
+  res.status(200).send({ msg: "获取收件箱成功", inBox: user.inBox });
+});
+
+module.exports = router;
