48bb51b2d81b88509140e2e88754a05ab4b84754
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-07-31 22:37:43 +0800

    fix:用户名不唯一性的问题

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -5,51 +5,52 @@ const userSchema = new mongoose.Schema({
     type: String, //类型
     required: true, //必填项
     minlength: 3, //最小值
-    maxlength: 8 //最大值
+    maxlength: 8, //最大值
+    unique: true, //唯一性
   },
   password: {
     type: String,
     required: true,
     minlength: 8,
-    maxlength: 200 //之所以设置这么大，是因为密码打算用哈希加密计算
+    maxlength: 200, //之所以设置这么大，是因为密码打算用哈希加密计算
   },
   email: {
     type: String,
     required: true,
     minlength: 4,
     maxlength: 20,
-    unique: true //唯一性，防止其他文档数据出现同样手机号码
+    unique: true, //唯一性
   },
   createDate: {
     type: Number,
     minlength: 8,
     maxlength: 16,
-    required: true
+    required: true,
   },
   isAdmin: {
     type: Boolean,
-    required: true
+    required: true,
   },
   isOnline: {
     type: Boolean,
-    required: true
+    required: true,
   },
   isValidate: {
     type: Boolean,
-    required: true
+    required: true,
   },
   friends: {
     type: Array,
-    required: true
+    required: true,
   },
   inBox: {
     type: Array,
-    required: true
+    required: true,
   },
   avatarUrl: {
     type: String,
-    required: true
-  }
+    required: true,
+  },
 });
 
 //导出模块
