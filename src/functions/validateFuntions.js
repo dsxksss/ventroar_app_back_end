0e1f01bff1ffb4fd41e2af8fd227f22dcd171068
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-08-01 17:50:57 +0800

    add:回贴RoarText数据格式验证函数

--- src/functions/validateFuntions.js
+++ src/functions/validateFuntions.js
@@ -112,6 +112,20 @@ const roarTextValidation = (data) => {
   return schema.validate(data);
 };
 
+/**
+ * @functions 回贴RoarText数据格式验证函数
+ * @return {boolean} 如果验证正确返回true
+ */
+const backRoarTextValidation = (data) => {
+  const schema = Joi.object({
+    textId: Joi.string().min(10).max(50).required(),
+    backText: Joi.string().min(3).max(510).required(),
+    isShowUserName: Joi.boolean().required(),
+  });
+  //返回验证结果
+  return schema.validate(data);
+};
+
 exports.createUserValidation = createUserValidation; //创建用户数据格式模板
 exports.createRoarTextValidation = createRoarTextValidation; //创建发泄帖子数据格式模板
 exports.signInValidation = signInValidation; //登录账户数据格式模板
@@ -120,4 +134,5 @@ exports.passwordValidation = passwordValidation; //密码格式模板
 exports.nameValidation = nameValidation; //昵称格式模板
 exports.likesValidation = likesValidation; //点赞格式模板
 exports.idValidation = idValidation; //id格式模板
-exports.roarTextValidation = roarTextValidation; //id格式模板
+exports.roarTextValidation = roarTextValidation; //修改宣泄帖格式模板
+exports.backRoarTextValidation = backRoarTextValidation; //回复宣泄帖格式模板
