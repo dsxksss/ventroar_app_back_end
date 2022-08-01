3ffe2d6524e05fb2d6ce8a63b7d567ba7009ca88
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-08-01 15:57:39 +0800

    change:简化代码

--- src/functions/sendBoxMsg.js
+++ src/functions/sendBoxMsg.js
@@ -1,7 +1,7 @@
 /**
  * @name 发送收件箱信件的封装函数
  * @param {string} msg 邮件内容
- * @param {string} isRead 是否已读 
+ * @param {string} isRead 是否已读
  * @param {string} msgType 信件类型
  */
 
@@ -9,16 +9,11 @@ exports.MsgType = {
   info: "info", //重要的通知
   unimportant: "unimportant", //不重要的信件
   warning: "warning", //低警告信件
-  error: "error" //高警告信件
+  error: "error", //高警告信件
 };
 
 async function sendBoxMsg({ msg, isRead = false, msgType }) {
-  this.push({
-    msg: msg,
-    msgType: msgType,
-    isRead: isRead,
-    date: Math.round(new Date() / 1000) //用户创建时间
-  });
+  this.push({ msg, msgType, isRead, date: Math.round(new Date() / 1000) }); //信件创建时间
 }
 
 exports.sendBoxMsg = sendBoxMsg;
