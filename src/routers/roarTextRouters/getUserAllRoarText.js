ed6b0dbcdc1863e69ac1ea0f20873caa6421e4b5
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-08-31 17:50:21 +0800

    chaneg:加工获取用户宣泄历史数据

--- src/routers/roarTextRouters/getUserAllRoarText.js
+++ src/routers/roarTextRouters/getUserAllRoarText.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { UserDB } = require("../../databases/userDB");
 const { RoarTextDB } = require("../../databases/roarTextDB");
 
 const auth = require("../../middlewares/auth");
@@ -6,8 +7,44 @@ const router = express.Router();
 
 router.get(`/`, [auth], async (req, res) => {
   try {
-    let texts = await RoarTextDB.find({ userId: req.userToken._id });
-    return res.status(200).send({ msg: "获取成功", result: texts });
+    let oldTexts = await RoarTextDB.find({ userId: req.userToken._id }).sort({
+      createDate: -1,
+    });
+
+    let usersId = [];
+    let newTexts = [];
+    oldTexts.forEach((roar) => usersId.push(roar.userId));
+    let users = await UserDB.find({ _id: { $in: [...usersId] } }).select({
+      name: 1,
+      email: 1,
+      avatarUrl: 1,
+    });
+
+    //加工数据
+    oldTexts.forEach((textItem) => {
+      users.forEach((item) => {
+        if (textItem.userId == item._id) {
+          newTexts.push({
+            _id: textItem._id,
+            text: textItem.text,
+            isPublic: textItem.isPublic,
+            isShowUserName: textItem.isShowUserName,
+            isCanComment: textItem.isCanComment,
+            likeUsers: textItem.likeUsers,
+            textImages: textItem.textImages,
+            textComments: textItem.textComments.length,
+            createDate: textItem.createDate,
+            smil: textItem.smil,
+            heart: textItem.heart,
+            userId: textItem.userId,
+            userName: item.name,
+            userEmail: item.email,
+            userAvatarUrl: item.avatarUrl,
+          });
+        }
+      });
+    });
+    return res.status(200).send({ msg: "获取用户宣泄历史数据成功", result: newTexts });
   } catch (e) {
     return res
       .status(408) //请求超时。客户端没有在服务器预备等待的时间内完成一个请求的发送。客户端可以随时再次提交这一请求而无需进行任何更改。
